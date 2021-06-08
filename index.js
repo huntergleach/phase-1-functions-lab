4744e13c923846e1f974ca836c1229e421bc5a98
huntergleach <hunter.leach21@gmail.com>
2021-06-08 13:01:32 -0500

    commit tree db60bb99c13161b2f3815311a41a62f7020ea5f3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const hq = 42
+
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > hq){
+        return (pickup-hq)}
+    else if (pickup < hq){
+        return (hq-pickup)}
+}
+
+function distanceFromHqInFeet(pickup) {
+    distanceFromHqInBlocks(pickup);
+    return (pickup * 264);
+
+}
+
+function distanceTravelledInFeet () {
+
+
+}
+
+function calculatesFarePrice () {
+
+
+}
+
